5ec437e463130cc8dc7bd751e806f1df796a6180
Michael Cetrulo <mail2samus@gmail.com>
2014-07-24 03:29:44 -0300

    implement positive filtering

--- src/dupes.py
+++ src/dupes.py
@@ -2,21 +2,27 @@
 """Find duplicate files
 
 Usage:
-  dupes.py [-e GLOB]... [-r REGEX]... [-R REGEX]... [PATH ...]
+  dupes.py [-i GLOB]... [-m REGEX]... [-M REGEX]...
+           [-e GLOB]... [-r REGEX]... [-R REGEX]...
+           [PATH ...]
   dupes.py (-h | --help)
   dupes.py --version
 
 Arguments:
-  PATH ...                          Scan the given directories or
-                                    current dir if omitted.
+  PATH ...                        Scan the given directories or
+                                  current dir if omitted.
 
 Options:
-  -e GLOB --exclude=GLOB            Exclude glob pattern from matches.
-  -r REGEX --exclude-re=REGEX       Exclude regex pattern from matches.
-  -R REGEX --iexclude-re=REGEX      Same but case-insensitive.
+  -i GLOB --include=GLOB          Show files matching these glob patterns.
+  -m REGEX --include-re=REGEX     Show files matching these regex patterns.
+  -M REGEX --iinclude-re=REGEX    Same but case-insensitive.
 
-  -h --help                         Show this screen.
-  --version                         Show version.
+  -e GLOB --exclude=GLOB          Exclude files matching these glob patterns.
+  -r REGEX --exclude-re=REGEX     Exclude files matching these regex patterns.
+  -R REGEX --iexclude-re=REGEX    Same but case-insensitive.
+
+  -h --help                       Show this screen.
+  --version                       Show version.
 """
 
 __version__ = '1.0.0'
@@ -30,43 +36,44 @@ from itertools import chain
 from docopt import docopt
 
 
-def exclude_name_factory(exclude_glob=None, exclude_re=None, iexclude_re=None):
-    if exclude_glob is None:
-        exclude_glob = []
+def test_name_factory(globs, regexs, iregexs):
+    regexs = [re.compile(regex) for regex in regexs]
+    iregexs = [re.compile(regex, re.IGNORECASE) for regex in iregexs]
 
-    test_exclude_glob = lambda name: any((
-        fnmatch(name, glob) for glob in exclude_glob
-    ))
+    test_globs = lambda name: (
+        fnmatch(name, glob) for glob in globs
+    )
+    test_regexs = lambda name: (
+        regex.search(name) for regex in regexs
+    )
+    test_iregexs = lambda name: (
+        regex.search(name) for regex in iregexs
+    )
 
-    if exclude_re is None:
-        exclude_re = []
-    exclude_re = [re.compile(regex) for regex in exclude_re]
-
-    test_exclude_re = lambda name: any((
-        regex.search(name) for regex in exclude_re
-    ))
-
-    if iexclude_re is None:
-        iexclude_re = []
-    iexclude_re = [re.compile(regex, re.IGNORECASE) for regex in iexclude_re]
-
-    test_iexclude_re = lambda name: any((
-        regex.search(name) for regex in iexclude_re
-    ))
-
-    return lambda name: any((
-        test_exclude_glob(name), test_exclude_re(name), test_iexclude_re(name)
+    return lambda name: any(chain(
+        test_globs(name), test_regexs(name), test_iregexs(name)
     ))
 
 
-def get_duped_filenames(target_dirs, **kwargs):
+def get_duped_filenames(target_dirs, include_patterns, exclude_patterns):
+    test_include_name_f = test_name_factory(**include_patterns)
+    test_exclude_name_f = test_name_factory(**exclude_patterns)
+
+    if any(include_patterns.values()):
+        test_name = lambda name: (
+            test_include_name_f(name) and not test_exclude_name_f(name)
+        )
+    else:
+        test_name = lambda name: (
+            not test_exclude_name_f(name)
+        )
+
     seen_names = defaultdict(set)
-    test_name_f = exclude_name_factory(**kwargs)
 
     walk_dirs = (os.walk(target) for target in target_dirs)
     for dirpath, dirnames, filenames in chain.from_iterable(walk_dirs):
         for filename in filenames:
-            if not test_name_f(filename):
+            if test_name(filename):
                 seen_names[filename].add(dirpath)
 
     return dict(
@@ -79,12 +86,20 @@ def get_duped_filenames(target_dirs, **kwargs):
 if __name__ == '__main__':
     arguments = docopt(__doc__, version=__version__)
 
-    kwargs = dict(
-        exclude_glob=arguments['--exclude'],
-        exclude_re=arguments['--exclude-re'],
-        iexclude_re=arguments['--iexclude-re'],
+    include_patterns = dict(
+        globs=arguments['--include'],
+        regexs=arguments['--include-re'],
+        iregexs=arguments['--iinclude-re'],
     )
-    duped_filenames = get_duped_filenames(arguments['PATH'] or ['.'], **kwargs)
+
+    exclude_patterns = dict(
+        globs=arguments['--exclude'],
+        regexs=arguments['--exclude-re'],
+        iregexs=arguments['--iexclude-re'],
+    )
+
+    duped_filenames = get_duped_filenames(arguments['PATH'] or ['.'],
+                                          include_patterns, exclude_patterns)
 
     for filename, dirpaths in duped_filenames.items():
         print(filename)

3303e3fa0ab8a6d4832f5f9383cc6ebc5909bd23
Michael Cetrulo <mail2samus@gmail.com>
2014-07-24 02:24:30 -0300

    implement exclusion flags

--- src/dupes.py
+++ src/dupes.py
@@ -2,22 +2,17 @@
 """Find duplicate files
 
 Usage:
-  dupes.py [-e GLOB]... [-E GLOB]...
-           [-r REGEX]... [-R REGEX]...
-           [PATH ...]
+  dupes.py [-e GLOB]... [-r REGEX]... [PATH ...]
   dupes.py (-h | --help)
   dupes.py --version
 
 Arguments:
   PATH ...                          Scan the given directories or
-                                    current dir if ommitted.
+                                    current dir if omitted.
 
 Options:
   -e GLOB --exclude=GLOB            Exclude glob pattern from matches.
-  -E GLOB --case-exclude=GLOB       Same but case-sensitive.
-
   -r REGEX --exclude-re=REGEX       Exclude regex pattern from matches.
-  -R REGEX --case-exclude-re=REGEX  Same but case-sensitive.
 
   -h --help                         Show this screen.
   --version                         Show version.
@@ -28,19 +23,40 @@ __version__ = '1.0.0'
 import os
 import re
 from collections import defaultdict
-from fnmatch import fnmatch, fnmatchcase
+from fnmatch import fnmatch
 from itertools import chain
 
 from docopt import docopt
 
 
-def get_duped_filenames(target_dirs):
+def exclude_name_factory(exclude_glob=None, exclude_re=None):
+    if exclude_glob is None:
+        exclude_glob = []
+
+    test_e_glob = lambda name: any((
+        fnmatch(name, glob) for glob in exclude_glob
+    ))
+
+    if exclude_re is None:
+        exclude_re = []
+    exclude_re = [re.compile(regex) for regex in exclude_re]
+
+    test_e_re = lambda name: any((
+        regex.search(name) for regex in exclude_re
+    ))
+
+    return lambda name: any((test_e_glob(name), test_e_re(name)))
+
+
+def get_duped_filenames(target_dirs, **kwargs):
     seen_names = defaultdict(set)
+    test_name_f = exclude_name_factory(**kwargs)
 
     walk_dirs = (os.walk(target) for target in target_dirs)
     for dirpath, dirnames, filenames in chain.from_iterable(walk_dirs):
         for filename in filenames:
-            seen_names[filename].add(dirpath)
+            if not test_name_f(filename):
+                seen_names[filename].add(dirpath)
 
     return dict(
         (filename, dirpaths)
@@ -52,7 +68,12 @@ def get_duped_filenames(target_dirs):
 if __name__ == '__main__':
     arguments = docopt(__doc__, version=__version__)
 
-    duped_filenames = get_duped_filenames(arguments['PATH'] or ['.'])
+    kwargs = dict(
+        exclude_glob=arguments['--exclude'],
+        exclude_re=arguments['--exclude-re'],
+    )
+    duped_filenames = get_duped_filenames(arguments['PATH'] or ['.'], **kwargs)
+
     for filename, dirpaths in duped_filenames.items():
         print(filename)
         for dirpath in dirpaths:
